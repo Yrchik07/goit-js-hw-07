1e9106128355d497b66d0f852f44251d8ffd36cd
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2023-12-19 12:03:23 +0200

    task-1 finish

--- js/task-01.js
+++ js/task-01.js
@@ -1,48 +1,12 @@
-{/* <ul id="categories">
-  <li class="item">
-    <h2>Animals</h2>
-    <ul>
-      <li>Cat</li>
-      <li>Hamster</li>
-      <li>Horse</li>
-      <li>Parrot</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Products</h2>
-    <ul>
-      <li>Bread</li>
-      <li>Parsley</li>
-      <li>Cheese</li>
-    </ul>
-  </li>
-  <li class="item">
-    <h2>Technologies</h2>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-      <li>Node.js</li>
-    </ul>
-  </li>
-</ul> */}
+const cat = document.querySelector('#categories');
+const catItem = document.querySelectorAll('.item');
+// console.log(catItem);
+console.log("Number of categories:", catItem.length);
 
-const x = document.querySelector('ul#categories');
-
-const q = x.childNodes;
-const w = x.querySelector('text');
-const y = x.children.length;
-
-
-// const q = y.forEach();
-// y.forEach(function (number, index) {
-//     console.log(`Index ${index}, value ${number}`);
-//   });
-
-// const y = x.textContent('h2');
-console.log(x);
-console.log(y);
-console.log(q);
-console.log(w);
-// console.log(x.textContent('h2'));
+catItem.forEach((item) =>{
+  const catTitle = item.querySelector("h2").textContent;
+  const catLi = item.querySelectorAll("li");
+  console.log("Category:", catTitle);
+  // console.log(item);
+  console.log("Elements:", catLi.length)
+})
