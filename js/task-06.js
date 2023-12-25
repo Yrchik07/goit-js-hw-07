5ece4f3aa68d2d95a25e491a98664737aebfdf2d
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2023-12-25 21:06:18 +0200

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,52 @@
+const controls = document.querySelector("#controls");
+const input = document.querySelector("#controls input");
+let output = 1;
+console.log(output);
+
+input.addEventListener("input", (event) => output += event);
+console.log(event)
+// const create = controls.childNodes;
+// console.log(create);
+
+const create = document.querySelector("#controls button");
+// console.log(create.textContent);
+const destroy = document.querySelector("#controls button");
+// console.log(create.textContent);
+
+// const destroy = document.querySelector("#boxes");
+const box = document.querySelector("#boxes");
+const element1 = document.createElement("div")
+
+
+
+element1.style.margin = "10px";
+element1.style.backgroundColor = "#d23bd5";
+element1.style.width = "30px";
+element1.style.height = "30px";
+
+const element2 = document.createElement("div")
+element2.style.margin = "10px";
+element2.style.backgroundColor = "#d23bd5";
+element2.style.width = "30px";
+element2.style.height = "30px";
+
+const element3 = document.createElement("div")
+element3.style.margin = "10px";
+element3.style.backgroundColor = "#d23bd5";
+element3.style.width = "30px";
+element3.style.height = "30px";
+
+const element4 = document.createElement("div")
+element4.style.margin = "10px";
+element4.style.backgroundColor = "#d23bd5";
+element4.style.width = "30px";
+element4.style.height = "30px";
+// element.classList.add("box")
+box.prepend(element1, element2, element3, element4);
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
