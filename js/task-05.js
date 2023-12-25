519d1b3d12ee171cc641cad3d879f80321dceb3c
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2023-12-25 16:57:42 +0200

    task-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,33 @@
+const button = document.querySelector(".change-color");
+const bColor = document.querySelector(".color");
+const bColor1 = document.querySelector("body");
+// console.log(bColor)
+
+
+// const textInput = document.querySelector("#name-input");
+// const output = document.querySelector("#name-output");
+// console.log(textInput)
+// console.log(output)
+// textInput.addEventListener("input", (event) => {
+//     if(event.currentTarget.value.trim()){
+//   output.textContent = event.currentTarget.value.trim();}
+// });
+
+
+
+
+
+button.addEventListener("click", () => {
+  bColor.textContent = getRandomHexColor()
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+});
+// bColor1.style.backgroundColor = "#800cad";
+
+// bColor1.style.backgroundColor = "getRandomHexColor()";
+
+console.log(bColor.textContent)
