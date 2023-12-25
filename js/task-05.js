ffd601f52869e8065b6bc26b5ea54c549f695f3c
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2023-12-25 19:05:57 +0200

    task-5 finish

--- js/task-05.js
+++ js/task-05.js
@@ -1,22 +1,6 @@
 const button = document.querySelector(".change-color");
 const bColor = document.querySelector(".color");
 const bColor1 = document.querySelector("body");
-// console.log(bColor)
-
-
-// const textInput = document.querySelector("#name-input");
-// const output = document.querySelector("#name-output");
-// console.log(textInput)
-// console.log(output)
-// textInput.addEventListener("input", (event) => {
-//     if(event.currentTarget.value.trim()){
-//   output.textContent = event.currentTarget.value.trim();}
-// });
-
-
-
-
-
 button.addEventListener("click", () => {
   bColor.textContent = getRandomHexColor()
 function getRandomHexColor() {
@@ -24,10 +8,7 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(bColor.textContent )
+ bColor1.style.backgroundColor = `${bColor.textContent}`;
 
 });
-// bColor1.style.backgroundColor = "#800cad";
-
-// bColor1.style.backgroundColor = "getRandomHexColor()";
-
-console.log(bColor.textContent)
