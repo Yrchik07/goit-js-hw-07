2d10c6017c935ecd9f507ec99bf8e71bd14d0689
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2023-12-25 15:21:26 +0200

    task-4 finish

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", funSubmit);
+
+function funSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+    
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  console.log(`email: ${email},
+password: ${password}`);
+  form.reset();
+}
