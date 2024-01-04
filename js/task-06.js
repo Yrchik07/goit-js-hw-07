661224f4583384abd46ffc1a475c4cc0fd54e7c0
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-01-04 20:43:31 +0200

    task-7 size problem

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#controls');
 const input = container.querySelector('input');
 const create = container.querySelector('button');
-const box = document.querySelector('#boxes');
+const boxContainer = document.querySelector('#boxes');
 
 // console.log(container);
 // console.log(input);
@@ -11,7 +11,7 @@ input.addEventListener('blur', onInputBlur);
 create.addEventListener('click', onClickBtn);
 function onClickBtn(){
   if(amount >= 1 & amount <= 100){
-    console.log(amount);
+    // console.log(amount);
     createBoxes(amount)
   }
 }
@@ -20,17 +20,27 @@ function onInputBlur(){
   amount = input.value;
 }
 
-// console.log(namber);
+// console.log(amount);
 function createBoxes(amount){
-console.log('hello')
+  boxContainer.innerHTML = '';
+  for (let i = 0; i < amount; i++) {
+    boxContainer.insertAdjacentHTML('beforeend', elemDiv())
+  }
+  
+// console.log(element);
+input.value = '';
 }
+function elemDiv(){
+  let width = 30 + 10;
+  let height = 30 + 10;
+  return `<div class="box" style="width: ${width}px;height: ${height}px;background-color: ${getRandomHexColor()};"></div>`
 
+}
+// console.log(elemDiv())
 
-
-
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+console.log(getRandomHexColor());
